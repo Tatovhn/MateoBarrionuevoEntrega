39305cc377a6b7fd2df8fe140a3549e290fb55e0
Tatovhn <112035251+Tatovhn@users.noreply.github.com>
2023-09-08 13:38:28 -0300

    actualización con mejora en storage

--- entregaFinal modificanding/main.js
+++ entregaFinal modificanding/main.js
@@ -6,7 +6,7 @@ class Producto{
   this.precio = precio;
 } 
 }
-
+// constructor de objeto pokemon
 class Pokemon{
   constructor(id,nombre,precio,img){
   this.id=id;
@@ -15,11 +15,9 @@ class Pokemon{
   this.img=img;
 } 
 }
-const Pokemones =[]
-
 
 // comienzo de el arreglo de productos
-const Productos =[
+let Productos =[
   {id:2,
    nombre:"pokebola",
    precio: 20
@@ -54,6 +52,10 @@ const Productos =[
    }
 ]
 
+localStorage.getItem("productos")?Productos=JSON.parse(localStorage.getItem("productos")):localStorage.setItem("productos",JSON.stringify(Productos));
+let Pokemones=[];
+localStorage.getItem("pokemones")?Pokemones=JSON.parse(localStorage.getItem("pokemones")):localStorage.setItem("pokemones",JSON.stringify(Pokemones));
+
 // carrusel
 let carrusel = document.getElementById("carrusel");
 let carrusel1 = document.getElementById("tarjeta1");
@@ -62,7 +64,7 @@ let divcar1prod= Math.floor(Math.random() * 5) + 1;
 console.log(divcar1prod)
 divcar1.innerHTML = `
 <div class="card bg-dark mx-auto border border-warning" style="width: 18rem;text-align: center;">
-    <img src="images/4811.jpg" class="card-img-top" alt="...">
+    <img src="icon/sale.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title red" id="nombreProd1"> Producto: ${Productos[divcar1prod].nombre}</h5>
       <h6 class="card-text" id="idProducto" type="number">id: ${Productos[divcar1prod].id}</h6>
@@ -82,7 +84,7 @@ let divcar2prod= divcar1prod + 1;
 
 divcar2.innerHTML = `
 <div class="card bg-dark mx-auto border border-warning" style="width: 18rem;text-align: center;">
-    <img src="images/4811.jpg" class="card-img-top" alt="...">
+    <img src="icon/sale.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title red" id="nombreProd1"> Producto: ${Productos[divcar2prod].nombre}</h5>
       <h6 class="card-text" id="idProducto" type="number">id: ${Productos[divcar2prod].id}</h6>
@@ -101,7 +103,7 @@ let divcar3prod= divcar2prod + 1;
 
 divcar3.innerHTML = `
 <div class="card bg-dark mx-auto border border-warning" style="width: 18rem;text-align: center;">
-    <img src="images/4811.jpg" class="card-img-top" alt="...">
+    <img src="icon/sale.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title red" id="nombreProd1"> Producto: ${Productos[divcar3prod].nombre}</h5>
       <h6 class="card-text" id="idProducto" type="number">id: ${Productos[divcar3prod].id}</h6>
@@ -112,7 +114,7 @@ divcar3.innerHTML = `
 `
 carrusel3.append(divcar3);
 let botont3 = document.getElementById(`${Productos[divcar3prod].id}`);
-botont3.addEventListener('click',() => comprar(Productos[divcar3prod].id));3
+botont3.addEventListener('click',() =>comprar(Productos[divcar3prod].id));3
 
 let dcto = false;
 let carrito=[];
@@ -120,6 +122,12 @@ const ProductosFinal =[];
 Productos.push(new Producto(1,"prueba",200));
 function comprar(id){
   let compra = Productos.find((item) => item.id===id);
+  let aidi = document.getElementById(id).parentNode;
+  aidi.parentElement.classList.remove('bg-dark');
+  aidi.parentElement.classList.add('bg-warning');
+  aidi.lastElementChild.classList.remove('btn-primary');
+  aidi.lastElementChild.classList.add('btn-success');
+  audio.play();
   carrito.push(compra);
   console.log(carrito);
 return carrito}
@@ -129,6 +137,7 @@ function ingresar(){
   let productoIng= document.getElementById('ingresoProducto').value;
   let precioIng= parseInt(document.getElementById('ingresoPrecio').value)
   Productos.push(new Producto (Math.max(...Productos.map(item => item.id))+1,productoIng,precioIng));
+  localStorage.setItem("productos",JSON.stringify(Productos));
   document.getElementById('ingresoProducto').value="";
   document.getElementById('ingresoPrecio').value="";
   console.log(Productos);  Toastify({
@@ -147,29 +156,39 @@ let lista = document.getElementById('lista');
 let template = document.getElementById('resultados');
 
 // boton buscar
-function buscar(){ 
+function buscar(e){ 
   lista.innerHTML="";
   carrusel.innerHTML="";
   let buscado = Productos.find(producto => producto.nombre === document.getElementById('searchProducto').value);
   console.log(buscado);
-  let clone = template.content.cloneNode(true);
-  clone.querySelector('h5').textContent=buscado.nombre;
-  clone.querySelector('h6').textContent= `Código de producto: ${buscado.id}`;
-  clone.querySelector('p').textContent=`$ ${buscado.precio}`;
-  clone.querySelector('button').setAttribute = ("id",buscado.id ) ;
-  lista.appendChild(clone)
+  let div = document.createElement('div');div.classList.add('d-inline-block');div.classList.add('mx-2');div.classList.add('m-t--1');
+  div.innerHTML= `
+  <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
+      <img src="icon/poke.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title red" id="nombreProd1"> Producto: ${buscado.nombre}</h5>
+        <h6 class="card-text" id="idProducto" type="number">id: ${buscado.id}</h6>
+        <h6 class="card-text" id="precio1" type="number" >$ ${buscado.precio}</h6>
+        <button id="${buscado.id}" class="btn btn-primary">Agregar</button>
+      </div>
+   </div> 
+`
+lista.append(div);
+let boton = document.getElementById(`${buscado.id}`);
+boton.addEventListener('click',() => comprar(buscado.id));
   document.getElementById('searchProducto').value="";
 }
 
 // mostrar todos los productos
 function inventario(){
+  productosDelInventario=JSON.parse(localStorage.getItem("productos"));
   lista.innerHTML="";
   carrusel.innerHTML="";
-  Productos.forEach((item)=>{
+  productosDelInventario.forEach((item)=>{
   let div = document.createElement('div');div.classList.add('d-inline-block');div.classList.add('mx-2');div.classList.add('m-t--1');
   div.innerHTML= `
   <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
-      <img src="images/4811.jpg" class="card-img-top" alt="...">
+      <img src="icon/poke.png" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title red" id="nombreProd1"> Producto: ${item.nombre}</h5>
         <h6 class="card-text" id="idProducto" type="number">id: ${item.id}</h6>
@@ -185,6 +204,12 @@ boton.addEventListener('click',() => comprar(item.id));
 //agregar al array de carrito provisorio el producto
 function comprar(id){
   let compra = Productos.find((item) => item.id===id);
+  let aidi = document.getElementById(id).parentNode;
+  aidi.parentElement.classList.remove('bg-dark');
+  aidi.parentElement.classList.add('bg-warning');
+  aidi.lastElementChild.classList.remove('btn-primary');
+  aidi.lastElementChild.classList.add('btn-success');
+  audio.play();
   carrito.push(compra);
   console.log(carrito); Toastify({
     text: "Producto agregado al carrito",
@@ -192,6 +217,7 @@ function comprar(id){
         style: {color: "black",background: "#fff",padding: "50px",},
    
   }).showToast();
+
 return carrito}
 })}
 
@@ -218,6 +244,12 @@ Pokeboton.addEventListener('click',() => comprar(poke.id));
 function comprar(id){
   let compra = Pokemones.find((item) => item.id===id);
   carrito.push(compra);
+  let aidi = document.getElementById(id).parentNode;
+  aidi.parentElement.classList.remove('bg-dark');
+  aidi.parentElement.classList.add('bg-warning');
+  aidi.lastElementChild.classList.remove('btn-primary');
+  aidi.lastElementChild.classList.add('btn-success');
+  audio.play();
   console.log(carrito); Toastify({
     text: "Pokemon agregado al carrito",
     duration: 3000,  gravity: "bottom", position: "center",
@@ -254,7 +286,7 @@ function carritoVer(){
  let div = document.createElement('div');div.classList.add('d-inline-block');div.classList.add('mx-2');div.classList.add('m-t--1');
  div.innerHTML= `
    <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
-       <img src="images/4811.jpg" class="card-img-top" alt="...">
+       <img src="icon/compra.png" class="card-img-top" alt="...">
        <div class="card-body">
          <h5 class="card-title red" id="nombreProd1"> Producto: ${item.nombre}</h5>
          <h6 class="card-text" id="idProducto" type="number">id: ${item.id}</h6>
@@ -267,7 +299,7 @@ function carritoVer(){
    })
   }
 // mostrar el carrito guardado, no está funcionando
-else if(carrito.length<=0 && localStorage.length>0){
+else if(carrito.length=0 && localStorage.length>0){
   lista.innerHTML="";carrusel.innerHTML="";
   carrito= JSON.parse(localStorage.getItem("carritoVerificado"));
 console.log(typeof carrito);
@@ -277,10 +309,10 @@ carrito.forEach((item)=>{
    <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
        <img src="images/4811.jpg" class="card-img-top" alt="...">
        <div class="card-body">
-         <h5 class="card-title red" id="nombreProd1"> Producto: ${JSON.stringify(item.nombre)}</h5>
-         <h6 class="card-text" id="idProducto" type="number">id: ${JSON.stringify(item.id)}</h6>
-         <h6 class="card-text" id="precio1" type="number" >$ ${JSON.stringify(item.precio)}</h6>
-         <button id="${JSON.stringify(item.id)}" class="btn btn-primary" onclick="quitarCarrito()">Quitar</button>
+         <h5 class="card-title red" id="nombreProd1"> Producto: ${item.nombre}</h5>
+         <h6 class="card-text" id="idProducto" type="number">id: ${item.id}</h6>
+         <h6 class="card-text" id="precio1" type="number" >$ ${item.precio}</h6>
+         <button id="${item.id}" class="btn btn-primary" onclick="quitarCarrito()">Quitar</button>
        </div>
     </div> 
  `
@@ -306,6 +338,7 @@ function quitarCarrito(id){
     },
     
   }).showToast();lista.innerHTML = "";
+  quitarSound.play();
   console.log(carrito);
 return carrito}
 
@@ -319,7 +352,8 @@ let preciofinalStorage = [];let preciofinal =0
 //verificar el carrito = guardarlo en storage baby
 function carritoVerificado () {
   localStorage.setItem("carritoVerificado",JSON.stringify(carrito));
- preciofinalStorage = localStorage.getItem("carritoVerificado");Toastify({
+ 
+ Toastify({
   text: "Tu carrito fue guardado, ya podés pagar",
   duration: 3000,
   gravity: "bottom", // `top` or `bottom`
@@ -333,11 +367,10 @@ function carritoVerificado () {
 
 
 
-
 // mostrar productos del carrito verificado y precio final
 function pagar(){
-if (JSON.parse(preciofinalStorage).length>0 && dcto){lista.innerHTML="";carrusel.innerHTML="";
-JSON.parse(preciofinalStorage).forEach((item)=>{
+if (carrito.length>0 && dcto){lista.innerHTML="";carrusel.innerHTML="";
+carrito.forEach((item)=>{
  preciofinal = preciofinal + parseInt(item.precio);
  productosCash =  productosCash + item.nombre +" | ";
 })
@@ -345,7 +378,7 @@ preciofinal= preciofinal - ((preciofinal*10)/100);
 //la tarjeta con el precio total
    let div = document.createElement('div');
    div.innerHTML= `
-     <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
+     <div class="card bg-dark d-block mx-auto border border-warning" style="width: 18rem;text-align: center;" id="tarj">
          <img src="images/about.png" class="card-img-top" alt="...">
          <div class="card-body">
          <h5 class="card-title red" id="nombreProd1"> Productos: ${productosCash}</h5>
@@ -359,8 +392,8 @@ preciofinal= preciofinal - ((preciofinal*10)/100);
    lista.append(div);
 
   }
- else if (JSON.parse(preciofinalStorage).length>0 && !dcto){lista.innerHTML="";carrusel.innerHTML="";
-JSON.parse(preciofinalStorage).forEach((item)=>{
+ else if ((carrito.length>0 && !dcto) || carrito.length<=0 && localStorage.length>0){lista.innerHTML="";carrusel.innerHTML="";
+JSON.parse(localStorage.getItem("carritoVerificado")).forEach((item)=>{
  
  preciofinal = preciofinal + parseInt(item.precio);
  productosCash =  productosCash + item.nombre +" | ";
@@ -392,6 +425,7 @@ console.log(preciofinal);
 }
 
 function Cashout(){
+  pagoSound.play();
   Swal.fire({
     title: 'GRACIAS POR TU POKECOMPRA',
     text: `Estás llevando ${productosCash} por $ ${preciofinal}`,
@@ -401,19 +435,15 @@ function Cashout(){
     cancelButtonColor: '#d33',
     confirmButtonText: 'COMPRAR'
   }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'LISTO',
-        'Tu compra fue registrada',
-        'success'
-      );
-      carrito=[]
-      carritoVerificado=[];
-        localStorage.clear();
-        location.reload()
-    }
-  })
-}
+    result.isConfirmed? (Swal.fire('LISTO','Tu compra fue registrada','success'),carrito=[],carritoVerificado=[],localStorage.clear(),location.reload())
+    :Swal.fire({
+      icon: 'error',
+      title: 'Ok, aún no está listo',
+      text: 'Mantendremos tus productos en el carrito',
+      footer: '<button onclick="carritoVer()">Ver carrito</button>'
+    })
+  }
+)}
 
 
 
@@ -423,22 +453,24 @@ function api(el){
   let name = document.getElementById('searchPoke').value;
   lista.innerHTML="";carrusel.innerHTML="";
   fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-   .then(response => response.json()).then(data => {
+   .then(response => response.json()).
+   then(data => {
   let poke = document.createElement('div');
         poke.innerHTML= `
-          <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;margin-left :5%;margin-top :5%;" id="tarj">
+          <div class="card bg-dark d-inline-block border border-warning" style="width: 18rem;text-align: center;mx-auto" id="tarj">
               <img src="${data.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="...">
               <div class="card-body">
-                <h5 class="card-title red" id="poke"> Pokemon: ${JSON.stringify(data.name)}</h5>
-                <h5 class="card-title red" id="poke"> Precio: ${JSON.stringify(data.base_experience)}</h5>
-                <button id="${JSON.stringify(data.id)}" onclick="comprar()"class="btn btn-primary pokecompra">Agregar</button>
+                <h5 class="card-title red" id="poke"> Pokemon: ${data.name}</h5>
+                <h5 class="card-title red" id="poke"> Precio: ${data.base_experience}</h5>
+                <button id="${data.id}" onclick="Pokeinventario()"class="btn btn-primary">Ver en el Pokeinventario</button>
                 </div>
            </div> 
         `
         
         lista.append(poke);
         let img =  data.sprites.other["official-artwork"].front_default;
-        Pokemones.push(new Pokemon(JSON.stringify(data.id),JSON.stringify(data.name),JSON.stringify(data.base_experience),img));
+        Pokemones.push(new Pokemon(data.id,data.name,data.base_experience,img));
+        localStorage.setItem("pokemones",JSON.stringify(Pokemones));
         name.value="";
       }).catch((err) =>{Swal.fire({
       icon: 'error',
@@ -472,4 +504,18 @@ function api(el){
       }).showToast();console.log(dcto)}
       else{dcto=false;
       console.log(dcto)}}
-  )},30000)
+  )},30000);
+
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/6253");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (button.className==="btn btn-primary" || button.className==="btn btn-success"){
+    audio.play();}
+  });
+});
+
+const quitarSound = new Audio("https://www.fesliyanstudios.com/play-mp3/7017");
+
+const pagoSound = new Audio("https://soundboardguy.com/wp-content/uploads/2022/06/ka-ching-1.mp3");
